aba6b7ed2029c93471f5e9ec90e2e651faf2e632
harsh1523 <91820977+harsh1523@users.noreply.github.com>
2025-12-25 10:47:16 +0530

    Initial Commit

---
+++ SwiftUINotesApp/SwiftUINotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINotesAppApp.swift
+//  SwiftUINotesApp
+//
+//  Created by Harsh on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
